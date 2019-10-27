53e70e1ef4a567a40baf9224fd3f812bc9e32d7f
Shama Hoque <shama.hoque@gmail.com>
2019-10-27 20:12:59 +0600

    second edition

--- server/express.js
+++ server/express.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import path from 'path'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compress from 'compression'
@@ -9,24 +8,7 @@ import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
 
-// modules for server side rendering
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import MainRouter from './../client/MainRouter'
-import { StaticRouter } from 'react-router-dom'
-
-import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles'
-import theme from './../client/theme'
-//end
-
-//comment out before building for production
-import devBundle from './devBundle'
-
-const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
-
-//comment out before building for production
-devBundle.compile(app)
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
@@ -38,32 +20,12 @@ app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
 
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
-
 // mount routes
 app.use('/', userRoutes)
 app.use('/', authRoutes)
 
-app.get('*', (req, res) => {
-  const sheets = new ServerStyleSheets()
-  const context = {}
-  const markup = ReactDOMServer.renderToString(
-    sheets.collect(
-          <StaticRouter location={req.url} context={context}>
-            <ThemeProvider theme={theme}>
-              <MainRouter />
-            </ThemeProvider>
-          </StaticRouter>
-        )
-    )
-    if (context.url) {
-      return res.redirect(303, context.url)
-    }
-    const css = sheets.toString()
-    res.status(200).send(Template({
-      markup: markup,
-      css: css
-    }))
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
 })
 
 // Catch unauthorised errors
--- template.js
+++ template.js
@@ -1,25 +1,12 @@
-export default ({markup, css}) => {
-    return `<!doctype html>
-      <html lang="en">
+export default () => {
+  return `<!doctype html>
+    <html lang="en">
         <head>
-          <meta charset="utf-8">
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          >
-          <title>MERN Skeleton</title>
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-          <style>
-              a{
-                text-decoration: none
-              }
-          </style>
+           <meta charset="utf-8">
+           <title>MERN Skeleton</title>
         </head>
-        <body style="margin:0">
-          <div id="root">${markup}</div>
-          <style id="jss-server-side">${css}</style>
-          <script type="text/javascript" src="/dist/bundle.js"></script>
+        <body>
+          <div id="root">Hello World</div>
         </body>
-      </html>`
+    </html>`
 }
